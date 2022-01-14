f741032d3d023243923d8057addd733592fcf111
dmontero7 <dmontero7@gmail.com>
2022-01-13 22:50:22 -0600

    message: se agregan los cambios hasta el listado 	modified:   clientes-app/src/app/app.component.html 	deleted:    clientes-app/src/app/app.component.spec.ts  modified:   clientes-app/src/app/app.module.ts 	new file:   clientes-app/src/app/clientes/cliente.ts 	new file:   clientes-app/src/app/clientes/clientes.component.html 	new file:   clientes-app/src/app/clientes/clientes.component.ts 	new file:   clientes-app/src/app/directiva/directiva.component.html 	new file:   clientes-app/src/app/directiva/directiva.component.ts 	new file:   clientes-app/src/app/footer/footer.component.css 	new file:   clientes-app/src/app/footer/footer.component.html 	new file:   clientes-app/src/app/footer/footer.component.ts 	new file:   clientes-app/src/app/header/header.component.html 	new file:   clientes-app/src/app/header/header.component.ts 	modified:   clientes-app/src/index.html 	modified:   clientes-app/src/styles.css 	modified:   clientes-app/tsconfig.json

---
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente } from "./cliente";
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+  listaCliente: Cliente[] = [
+    {id:1,nombre:'Daniel',apellido:'Montero',email:'dmontero7@gmail.com',createdAt:'13/01/2020'},
+    {id:2,nombre:'Danilo',apellido:'Montero',email:'danielmonterobadilla@gmail.com',createdAt:'13/01/2020'},
+    {id:3,nombre:'Marta',apellido:'Badilla',email:'mardan@gmail.com',createdAt:'13/01/2020'},
+    {id:4,nombre:'Sandra',apellido:'Montero',email:'spmontero@gmail.com',createdAt:'13/01/2020'},
+    {id:5,nombre:'Angélica',apellido:'Cruz',email:'angelcg27@gmail.com',createdAt:'13/01/2020'},
+    {id:6,nombre:'Sharom',apellido:'Murillo',email:'sharommm@gmail.com',createdAt:'13/01/2020'},
+    {id:7,nombre:'Maribel',apellido:'Cruz',email:'maricruz@gmail.com',createdAt:'13/01/2020'},
+    {id:8,nombre:'Fabian',apellido:'Mena',email:'famena@gmail.com',createdAt:'13/01/2020'},
+    {id:9,nombre:'Carlos',apellido:'Cruz',email:'ccruz@gmail.com',createdAt:'13/01/2020'},
+    {id:10,nombre:'Candre',apellido:'González',email:'candreg@gmail.com',createdAt:'13/01/2020'}
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
